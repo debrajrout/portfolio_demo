da3f399579a5a8867c962c1f1195d3bc47735092
debrajrout <debrajrout28@gmail.com>
2024-01-07 19:59:29 +0530

    first commit

---
+++ components/sub/HeroContent.tsx
@@ -0,0 +1,79 @@
+"use client";
+
+import React from "react";
+import { motion } from "framer-motion";
+import {
+  slideInFromLeft,
+  slideInFromRight,
+  slideInFromTop,
+} from "@/utils/motion";
+import { SparklesIcon } from "@heroicons/react/24/solid";
+import Image from "next/image";
+
+const HeroContent = () => {
+  return (
+    <motion.div
+      initial="hidden"
+      animate="visible"
+      className="flex flex-row items-center justify-center px-20 mt-40 w-full z-[20]"
+    >
+      <div className="h-full w-full flex flex-col gap-5 justify-center m-auto text-start">
+        <motion.div
+          variants={slideInFromTop}
+          className="Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9]"
+        >
+          <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
+          <h1 className="Welcome-text text-[13px]">welsome to my portfolio</h1>
+        </motion.div>
+
+        <motion.div
+          variants={slideInFromLeft(0.5)}
+          className="flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto"
+        >
+          <span>
+            Crafting
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
+              {" "}
+              the best{" "}
+            </span>
+            project exprience
+          </span>
+        </motion.div>
+
+        <motion.p
+          variants={slideInFromLeft(0.8)}
+          className="text-lg text-gray-400 my-5 max-w-[600px]"
+        >
+          As a versatile Full Stack Developer and DBA, I bring extensive
+          expertise honed through successful freelance projects on platforms
+          like Upwork, complemented by a specialization in SEO. Proficient in an
+          array of technologies such as React, Next.js, Node.js, Three.js, along
+          with database management skills encompassing MySQL, MongoDB,
+          PostgreSQL, and seamless integration with OpenAI APIs. I actively
+          contribute to open-source initiatives and excel in dynamic
+          problem-solving utilizing C++ Data Structures and Algorithms.
+        </motion.p>
+        <motion.a
+          variants={slideInFromLeft(1)}
+          className="py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px]"
+        >
+          Learn More!
+        </motion.a>
+      </div>
+
+      <motion.div
+        variants={slideInFromRight(0.8)}
+        className="w-full h-full flex justify-center items-center"
+      >
+        <Image
+          src="/mainIconsdark.svg"
+          alt="work icons"
+          height={650}
+          width={650}
+        />
+      </motion.div>
+    </motion.div>
+  );
+};
+
+export default HeroContent;
